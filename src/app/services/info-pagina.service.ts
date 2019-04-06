f824a7320742294666f38cc3a725b7f558324453
Javito spain <jaavni@gmail.com>
2019-04-06 20:17:00 +0200

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { InfoEquipo } from '../interfaces/info-equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,23 +9,42 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: InfoEquipo = {};
   cargada = false;
 
   constructor(private http: HttpClient) {
 
     //console.log('Funciona');
+    this.cargarInfo();
+    this.cargarEquipo();
+   }
 
-    // Leer fichero JSON
+   private cargarInfo(){
+     //https://angular-html-98995.firebaseio.com/equipo.json
+
+         // Leer fichero JSON
     this.http.get('assets/data/data-pagina.json')
-      .subscribe ( (resp: InfoPagina) => {
+    .subscribe ( (resp: InfoPagina) => {
 
+      this.cargada = true;
+      this.info = resp;
+      //console.log(resp.email);
+      //console.log( resp['web'] );
+      //console.log( resp['twitter'] );
+    })
+  }
+
+    private cargarEquipo(){
+
+      this.http.get('https://angular-html-98995.firebaseio.com/equipo.json')
+      .subscribe ( (resp: InfoEquipo) => {
+  
         this.cargada = true;
-        this.info = resp;
-        console.log(resp.email);
+        this.equipo = resp;
+        //console.log(resp);
         //console.log( resp['web'] );
         //console.log( resp['twitter'] );
       })
-
 
    }
 }
