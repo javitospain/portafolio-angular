667c014a230c96c5706dd0e1a8529e26a5e4768b
Javito spain <jaavni@gmail.com>
2019-04-06 19:03:09 +0200

    Fin sección 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,12 @@
+
+
+export interface InfoPagina{
+    titulo?: string;
+    email?: string;
+    nombreCorto?: string;
+    web?: string;
+    facebook?: string;
+    twitter?: string;
+    Equipo_trabajo?: any[];
+}
+
